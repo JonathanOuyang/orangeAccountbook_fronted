ae90a1b8ac4989b4304a54e1a1ae3694e94b7d7b
Jonathan <479305102@qq.com>
2018-12-15 20:53:09 +0800

    feature: 增加账单数字键盘输入功能

--- src/views/AddMoney.vue
+++ src/views/AddMoney.vue
@@ -18,7 +18,7 @@
                 </div> -->
                 <div class="account-name">支付宝</div>
             </div>
-            <div class="addMoney-val-money">¥80.00</div>
+            <div class="addMoney-val-money">￥{{value}}</div>
         </div>
         <van-swipe :autoplay="0" indicator-color="#f6717d">
             <van-swipe-item>
@@ -38,7 +38,7 @@
                 <icon name="rili30"></icon>
             </div>
             <van-cell-group>
-                <van-field v-model="value" placeholder="请输入用户名" />
+                <van-field v-model="desc" placeholder="请输入备注" />
             </van-cell-group>
         </div>
         <div class="addMoney-numberKeyborad">
@@ -47,115 +47,160 @@
             extra-key="."
             @blur="show = false"
             theme="custom"
+            @input="handleInputNumber"
+            @delete="handleDeleteNumber"
             />
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'addMoney',
-        data(){
-            return {
-                value: ''
-            }
+export default {
+  name: "addMoney",
+  data() {
+    return {
+      intStack: [],
+      floatStack: [],
+      isInputInt: true,
+			desc: ''
+    };
+  },
+  computed: {
+    value() {
+      const hasInt = Boolean(this.intStack.length),
+      		  hasFloat = Boolean(this.floatStack.length);
+      if (hasInt || hasFloat) {
+        if (!hasInt) {
+          return `0.${this.floatStack.join("")}`;
+        } else if (!hasFloat) {
+          return `${this.intStack.join("")}`;
         }
+				return `${this.intStack.join("")}.${this.floatStack.join("")}`;
+      } else return '0'
     }
+  },
+  methods: {
+    handleInputNumber(key) {
+      if (key == ".") this.isInputInt = false;
+      else {
+        if (this.isInputInt) {
+          this.intStack.push(key);
+        } else {
+          if (this.floatStack.length <= 1) this.floatStack.push(key);
+        }
+      }
+    },
+    handleDeleteNumber() {
+			const hasInt = Boolean(this.intStack.length),
+      		  hasFloat = Boolean(this.floatStack.length);
+			if (hasFloat){
+				this.floatStack.pop();
+				if(hasFloat){
+					this.isInputInt = true;
+				}
+			} else {
+				if(hasInt){
+					this.intStack.pop();
+				}
+			}
+		}
+  }
+};
 </script>
 
 <style lang="less">
-    @import "../assets/variable.less";
-    #view-addMoney {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;
-        color: @primaryTextColor;
-        background-color: #fff;        
-        &::after {
-            content: '';
-            width: 100%;
-            height: 216px;
-        }
+@import "../assets/variable.less";
+#view-addMoney {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  color: @primaryTextColor;
+  background-color: #fff;
+  &::after {
+    content: "";
+    width: 100%;
+    height: 216px;
+  }
+}
+.addMoney-header {
+  padding: 12 / @rem 18 / @rem;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid @dividerColor;
+  &-icon {
+    font-size: 18 / @rem;
+  }
+  &-tab {
+    flex: 1;
+  }
+}
+.addMoney-val {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70 / @rem;
+  border-bottom: 1px solid @dividerColor;
+  &-account {
+    display: flex;
+    align-items: center;
+    padding: 0 40 / @rem 0 20 / @rem;
+    height: 60%;
+    border-right: 1px solid @dividerColor;
+    font-size: 20 / @rem;
+    .account-icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 36 / @rem;
+      height: 36 / @rem;
+      border-radius: 4 / @rem;
+      background: #2873ff;
+      color: #fff;
+      font-size: 36 / @rem;
     }
-    .addMoney-header {
-        padding: 12/@rem 18/@rem;
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid @dividerColor;
-        &-icon {
-            font-size: 18/@rem;
-        }
-        &-tab {
-            flex: 1;
-        }
-    }
-    .addMoney-val {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 70/@rem;
-        border-bottom: 1px solid @dividerColor;
-        &-account {
-            display: flex;
-            align-items: center;
-            padding: 0 40/@rem 0 20/@rem;
-            height: 60%;
-            border-right: 1px solid @dividerColor;
-            font-size: 20/@rem;
-            .account-icon {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                width: 36/@rem;
-                height: 36/@rem;
-                border-radius: 4/@rem;
-                background: #2873ff;
-                color: #fff;
-                font-size: 36/@rem;
-            }
-        }
-        &-money {
-            font-size: 36/@rem;
-            padding: 0 12/@rem;
-        }
-    }
+  }
+  &-money {
+    font-size: 36 / @rem;
+    padding: 0 12 / @rem;
+  }
+}
 
-    .van-swipe{
-        flex:1;
+.van-swipe {
+  flex: 1;
 
-        .addMoney-type {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around;
-            height: 90%;
-            padding: 12/@rem 18/@rem 16/@rem;
-            &-item {
-                width: 20%;
-                margin: 6/@rem 0;
-            }
-        }
+  .addMoney-type {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    height: 90%;
+    padding: 12 / @rem 18 / @rem 16 / @rem;
+    &-item {
+      width: 20%;
+      margin: 6 / @rem 0;
     }
+  }
+}
 
-    .addMoney-infoBar {
-        display: flex;
-        align-items: center;
-        background: #fff;
-        // border-top: 1px solid @dividerColor;
-        .addMoney-infoBar-date {
-            padding: 0 16px;
-            border-right: 1px solid @dividerColor;
-            .iconfont {
-                font-size: 26px;
-            }
-        }
+.addMoney-infoBar {
+  display: flex;
+  align-items: center;
+  background: #fff;
+  // border-top: 1px solid @dividerColor;
+  .addMoney-infoBar-date {
+    padding: 0 16px;
+    border-right: 1px solid @dividerColor;
+    .iconfont {
+      font-size: 26px;
     }
+  }
+}
 
-    .addMoney-tagBar {
-        display: flex;
-        align-items: center;
-        height: 32px;
-        width: 100%;
-        border-top: 1px solid @dividerColor;
-    }
+.addMoney-tagBar {
+  display: flex;
+  align-items: center;
+  height: 32px;
+  width: 100%;
+  border-top: 1px solid @dividerColor;
+}
 </style>
